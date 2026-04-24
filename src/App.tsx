8f5d706e002a454688a846e3cb380b303a45b712
Badr-eddine BEN EL MOSTAFA <be.benelmostafa@eigsi.org>
2026-04-24 16:12:08 +0100

    first jet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
 import { LandingPage } from './pages/LandingPage'
 import { ResourcesPage } from './pages/ResourcesPage'
 
 function App() {
   const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [location.pathname])
 
   return (
     <AnimatePresence mode="wait">
